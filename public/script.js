1285a1435f0154b7888184729c4242b15a9bdf1c
ArameTerteryan <arame.terteryan@tumo.org>
2024-01-26 21:24:56 +0400

    take info from db

--- public/script.js
+++ public/script.js
@@ -1,6 +1,13 @@
-// function sendData(){
-//     let nameInput = document.getElementById('name').value
-//     let ageInput = document.getElementById('age').value
+let usPassword = document.getElementById('usPassword');
+let usName = document.getElementById('usName');
+let usEmail = document.getElementById('usEmail');
 
-//    console.log(nameInput.value, ageInput.value);
-// }
+function getVal() {
+    var response =  fetch("http://localhost:3000/addName", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name: usName.value, password: usPassword.value, email:usEmail.value })
+    })
+}
--- server.js
+++ server.js
@@ -1,43 +1,52 @@
 var express = require("express");
+var path = require("path");
+const bodyParser = require("body-parser");
 var app = express();
-const bodyParser = require('body-parser');
-let path = require('path');
 
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use(express.static('public'));
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname,'./public/form.html'));
+const mongoose = require("mongoose");
+const connectionString =
+  "mongodb+srv://arameterteryan:Arame1234@cluster0.1dtngnw.mongodb.net/sample_mflix";
 
+  mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'Connection error:'));
+  db.once('open', async () => {
+  try {
+  let result = await mongoose.connection.db.collection('theaters').find({'location.address.city':'Bloomington'}).toArray()
+  res.render('../public/form.ejs', {
+  obj: result
+  });
+  } catch (error) {
+  console.error('Error retrieving movies:', error);
+  } finally {
+  mongoose.connection.close();
+  }
+  })
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  let info = [
+    { name: "Sammy", organization: "DigitalOcean", birth_year: 2012 },
+    { name: "Tux", organization: "Linux", birth_year: 1996 },
+    { name: "Moby Dock", organization: "Docker", birth_year: 2013 },
+  ];
+  res.render("../public/form.ejs", {
+    obj:info
+  });
 });
 
-app.post('/addName', (req, res) => {
-    const name = req.body.name;
-    const age = req.body.age;
-    console.log('Received data:', name , age);
-    res.redirect('/')
+app.post("/addName", async (req, res) => {
+  const name = req.body.name;
+  const password = req.body.password;
+  const email = req.body.email;
 });
 
-
-
-app.listen(3000, function(){
-   console.log("Example is running on port 3000");
-});
-
-const mongoose = require('mongoose');
-
-// Replace the connection string with your MongoDB connection string
-const connectionString = 'mongodb+srv://arameterteryan:Arame1234@cluster0.1dtngnw.mongodb.net/?retryWrites=true&w=majority';
-
-
-// Connect to MongoDB
-mongoose.connect(connectionString, { useUnifiedTopology: true });
-
-// Check the connection
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', () => {
-console.log('Connected to MongoDB!');
-// You can add additional code here for testing or other operations
-// Make sure to close the connection when you're done
-mongoose.connection.close();
+app.listen(3000, function () {
+  console.log("Example is running on port 3000");
 });
